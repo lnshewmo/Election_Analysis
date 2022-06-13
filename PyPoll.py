81dc865548a908de1f394e11e0030be259291d7a
Laura Miller <laura.shewmon@gmail.com>
2022-06-12 22:48:13 -0400

    updated to end of module 3.5.3

--- PyPoll.py
+++ PyPoll.py
@@ -15,6 +15,16 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# initialize an ACCUMULATOR variable (like an index)
+# initialize a total vote counter
+# placement is before opening the file to read
+total_votes = 0
+
+# declare a new list for candidate data
+candidate_options = []
+# declare a dictionary to hold the {names:votes}
+candidate_votes = {}
+
 # Open the election results and read the file
 with open(file_to_load) as election_data:
 
@@ -23,9 +33,35 @@ with open(file_to_load) as election_data:
 
 # print the header (verify the first row is the header)
     headers = next(file_reader)
-    print(headers)
+    # print(headers) # activate this to see them
 
 # Print each row in the CSV file
-   for row in file_reader:
-        print(row) # prints the whole file
-       # print(row[0]) # Prints just the first column of each row
+    for row in file_reader:
+            # print(row) # prints the whole file
+            # print(row[0]) # Prints just the first column of each row
+
+            # add to the total_vote count
+            total_votes += 1
+            
+            # get the candidate name from each row[2]
+            candidate_name = row[2]
+
+            # if the candidate name does not match any current name
+            if candidate_name not in candidate_options:
+
+                # append candidate name to the candidate options list
+                candidate_options.append(candidate_name) # print(candidate_options) here gives a list of 3 names
+
+                # make the candidate names the key for {candidate vote} and set count to 0
+                candidate_votes[candidate_name] = 0
+
+            # add votes to each name
+            candidate_votes[candidate_name] += 1
+
+#print the candidate list
+print(candidate_options)
+
+# print the total_votes
+print(total_votes)
+
+print(candidate_votes)
